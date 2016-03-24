0b640febe0154934281659fd8a43aa5d10d50fea
jnvm <jon.nevermind@gmail.com>
2016-03-23 22:40:47 -0500

    init
    init

---
+++ dbq.js
@@ -0,0 +1,125 @@
+module.exports=function init(MYSQL,opts){
+	"tisnthings async".split(" ").map(x=>global[x]=require(x))
+	var db={
+		 verbose:1
+		,log:(query,rows,queryLine,took,db)=>{
+			console.log(`query in ${took}s:`,query.sql)
+		}
+		,query:(flow,...queryInfo)=>{
+			flow=flow||'series'
+
+			var callback=queryInfo.pop()
+			//did I assume there was a callback when there wasn't?
+			if(!tis(callback,aFxn)){
+				queryInfo.push(callback)
+				callback=false
+			}
+			//successive sets are "sql",[sub],"sql",[sub]
+			var querySets=[]
+				,set=null
+				,fxns=[]
+				,results=[]
+				,isVerbose=db.verbose
+				,lastItemIndex=queryInfo.length-1
+				,queryLine=isVerbose ? 
+					(new Error("!").stack).split('\n').filter((line,i)=>i && !line.match(/query\.queryInfo\.forEach\.querySets/))[0].split(":")[1]
+					: false
+	
+			//group into {sql:"",sub:[]} sets to call in desired flow
+			queryInfo.forEach((v,i)=>{
+				if(tis(v,"")){
+					if(set) querySets.push(set)//append the previous one that didn't have a substitution
+					set={sql:v}
+					if(lastItemIndex==i) querySets.push(set)
+				}
+				else if(tis(v,[])){
+					if(v.length){
+						//if I want to say "...where x in (?)",[ [] ] <- and I supply an empty array for the subtitution, it will err.  Replace this with a null, where it returns no results w/o erring
+						v=v.map(x=> tis(x,[]) && x.length==0 ? null :x )
+						set.substitute=v
+					}
+					querySets.push(set)
+					set=null
+				}
+			})
+
+			//form into async input function set
+			querySets=querySets.map((qset,i)=>{
+				//note connections are lazily retained, so this won't have real overhead once the pool is filled
+				return next=>{
+					var t1=Date.now()
+					MYSQL.getConnection((err,dbi)=>{
+						if(err) throw Error(err)
+						var query=dbi.query(qset.sql,qset.substitute,(err,rows)=>{
+							dbi.release()//done querying
+							var t2=Date.now()
+								,took=((t2-t1)/1000).toFixed(3)
+							if(isVerbose) db.log(query,rows,queryLine,took,db)
+							//handle error better...
+							if(err) console.log("BAD QUERY! this one:",query.sql,err)
+							else{
+								//did you hint you only want one row? If so, don't return a row, just the object
+								var wantOnlyOne=!!qset.sql.match(/limit\s+1\s*$/i)
+								results[i]=wantOnlyOne ? rows[0] : rows
+								
+								//further, if you're a single-key, return value.
+								if(wantOnlyOne){
+									var k=Object.keys(results[i])
+									if(k.length==1)
+										results[i]=results[i][k[0]]
+								}
+							}
+							next(err,err?[]:results)
+						})
+					})
+				}
+			})
+			
+			//call in desired flow & synchronicity, then place results as params to callback in original sequence provided.  This lets writer usefully name result vars in callback
+
+			if(callback)
+				return async[flow]( ...[
+					querySets
+					, x => callback(...results)
+				])
+			else
+				return new Promise((resolve,reject)=>{
+					async[flow]( ...[
+						querySets
+						, (err,asyncResults) =>{
+							if(err){
+								console.log(err)
+								reject(err)
+							}
+							resolve(results)
+						}
+					])
+				})
+		}
+		,qs:(...a)=>db.query(...(['series'  ].concat(a)) )
+		,series:(...a)=>db.qs(...a)
+		,qp:(...a)=>db.query(...(['parallel'].concat(a)) )
+		,parallel:(...a)=>db.qp(...a)
+		,q :(...a)=>db.qp(...a)
+		,table:{}//populated by schemize
+		,schemize:(done)=>{
+			done=done||aFxn
+			//actually ask the db what it has
+	  		db.q("select table_catalog,table_schema,table_name,column_name,ordinal_position,column_default,is_nullable,data_type,character_maximum_length,character_octet_length,numeric_precision,numeric_scale,character_set_name,collation_name,column_type,column_key,extra,privileges,column_comment from information_schema.columns where table_schema=?",[MYSQL.config.connectionConfig.database],tables=>{
+				tables.forEach(tbl=>{
+					if (!(tbl.table_name in db.table)) db.table[tbl.table_name]={}
+					if (!(tbl.column_name in db.table[tbl.table_name])) db.table[tbl.table_name][tbl.column_name]={}
+					db.table[tbl.table_name][tbl.column_name]=tbl
+				})
+				db.q("set global group_concat_max_len=65536",done)
+			})
+		}
+	}
+	
+	Object.assign(db,opts)
+	
+	//allow db("select ...") in addition to db.q("select ...")
+	Object.assign(db.q,db)
+	
+	return db.q
+}
---
+++ readme.md
@@ -0,0 +1,84 @@
+# dbq
+
+`dbq` = (`node-mysql` + `async` for batch execution & flow control) / (a preference for brevity &times; medium naiveté).
+
+Example: two queries, executed in parallel, two results:
+```javascript
+db("select * from ricks order by rickness desc limit 1"
+,"select * from mortys where dim=? order by mortyness desc limit 1",["c-137"]
+,(rickest,mortyest)=>/*fiddle*/)
+```
+
+### [Callbacks or Promises](#callbacks-or-promises)
+
+Pass a function as the last input, and that will receive query results as inputs in the order supplied:
+```javascript
+db("select * from user where name=?",['morty']
+,"select name,volume from dims where dim=?",['c-137']
+,(morty,dim)=>{/*fiddle*/})
+```
+
+If the last input isn't a function, a promise is returned, so is `then`able:
+```javascript
+db("select * from jerrys where dim=?",["c-137"]
+,"select * from ricks where dim=?",["J19ζ7"]
+).then(([jerry,doofusRick])=>{//a promise resolves to 1 value, but es6 destructuring can separate them
+   	//fiddle
+})
+//if it's thenable, you can catch, too
+.catch(errorHandler)
+//but it's already going to log out when errors happen anyway
+```
+### [Series or Parallel](#series-or-parallel)
+It can execute queries in series or parallel (assuming you have connection pooling on).
+```javascript
+//Parallel looks like this:
+db(	//could also have been db.parallel or db.qp or db.q
+	 "select * from user"
+	,"select * from book"
+	,"select * from dinosaur"
+).then(([users,books,dinosaurs])=>{/*fiddle*/})
+
+//series would be:
+db.qs( //or db.series
+	 "update cat set living=false"
+	,"update treaty set active=true where title='Spider Peace'"
+	,"insert into cat2 select * from cat where living=false"
+)
+```
+[node-mysql's ?-substitution syntax](https://github.com/felixge/node-mysql#escaping-query-values) is also allowed adjacently, as needed:
+```javascript
+db(	 "select * from grandpa where name=?",["rick"]
+	,"select * from council"//note no substitution needed here, so no [] is supplied
+	,"select * from morty where ?",[{alignment:"evil"}]
+	,"select * from dinosaur"
+).then(fiddle)
+```
+
+### [Return Shortcuts](#return-shortcuts)
+Queries are often performed to retrieve single value results, not arrays of objects.
+
+If you end a query with `limit 1`, it will take that one result out of its result `[]`, returning just the row `{}`.
+
+If you _also_ supply only one `select` clause column, the result will be just that `value`, not a `{key:value}`.
+
+### [Schemize](#schemize)
+If your credentials have `information_schema` access, `db.schemize()` will query it and put a representation of the database's tables and their columns at `db.table` for easy referencing elsewhere in code.
+
+### [Setup & Options](#setup-options)
+Any key:value passed to the `db` options object is `Object.assign`ed to `db`, so will overwrite defaults. Useful to create your own logging.  For example, I like to add an `ellipsize` option to it & the logger so I can see partial or full queries if debugging.
+```javascript
+var mysql=require("mysql").createPool({
+               host:'x',user:'x',password:'x',database:'x'
+              ,useConnectionPooling:true//allow parallel querying!               
+              ,connectionLimit:16
+              ,connectTimeout:15*60*1000
+          })
+	,db=require("dbq")(mysql,{//pass in node-mysql initialized above, then an options {}
+    	//option:[default]
+        ,verbose:true// console.log queries as they happen?
+    	,log:(query,rows,queryLine,took,db)=>{//maybe you want to customize how queries are logged
+        	console.log(`query in ${took}s:`,query.sql)
+        }
+    })
+```
